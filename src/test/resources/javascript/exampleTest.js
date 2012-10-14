2a038abe5d9232dec62d7f7807d937f4c8699e5f
Kevin Clark <kevinc@greplin.com>
2012-10-14 12:52:48 -0700

    Fix typo

--- src/test/resources/javascript/exampleTest.js
+++ src/test/resources/javascript/exampleTest.js
@@ -2,7 +2,7 @@ load('script/example');
 
 function testThatGETShouldReturnParamsWithNoPostData() {
   let params = {'foo': 'bar'};
-  let result = GET(params));
+  let result = GET(params);
   Assert.assertEquals("GET", result.method);
   Assert.assertEquals(params, result.params);
   Assert.assertNull(nullable(result.postdata));
